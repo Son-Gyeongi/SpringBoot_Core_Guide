ee66e10a5d7c27140c618da9886119e66b841c32
Son-Gyeongi <thsruddl77@gmail.com>
2024-04-02 13:32:14 +0900

    @RequestMapping을 사용한 메서드 구현

---
+++ api/src/main/java/com/springboot/api/controller/GetController.java
@@ -0,0 +1,19 @@
+package com.springboot.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/get-api")
+public class GetController {
+
+    /**
+     * @RequestMapping을 사용한 메서드 구현
+     */
+    // http://localhost:8080/api/v1/get-api/hello
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String getHello() {
+        return "Hello World";
+    }
+}
