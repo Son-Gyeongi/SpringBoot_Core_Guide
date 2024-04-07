6b18d8e8284a0d7f34bcc1f79ab8543406ddcf4c
Son-Gyeongi <thsruddl77@gmail.com>
2024-04-07 15:54:26 +0900

    6장 - 리포지토리 인터페이스 생성

---
+++ jpa/src/main/java/com/springboot/jpa/data/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.springboot.jpa.data.repository;
+
+import com.springboot.jpa.data.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// Product 테이블과 매핑되는 Product 엔티티에 대한 인터페이스
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
