7fa7b7b057462c8374ae39ab8ed6a244ef964319
Son-Gyeongi <thsruddl77@gmail.com>
2024-04-16 17:38:18 +0900

    7장 테스트 코드 - 설정 완료

---
+++ test/src/main/java/com/springboot/test/service/ProductService.java
@@ -0,0 +1,18 @@
+package com.springboot.test.service;
+
+import com.springboot.test.data.dto.ProductDto;
+import com.springboot.test.data.dto.ProductResponseDto;
+
+/**
+ * DAO에서 구현한 기능을 서비스 인터페이스에서 호출해 결괏값을 가져오는 작업을 수행하도록 설계
+ */
+public interface ProductService {
+
+    ProductResponseDto getProduct(Long number);
+
+    ProductResponseDto saveProduct(ProductDto productDto);
+
+    ProductResponseDto changeProductName(Long number, String name) throws Exception;
+
+    void deleteProduct(Long number) throws Exception;
+}
