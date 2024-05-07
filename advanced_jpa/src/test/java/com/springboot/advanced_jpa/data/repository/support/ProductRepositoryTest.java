3ff353c142d9b041613b5343019117bc9136324d
Son-Gyeongi <thsruddl77@gmail.com>
2024-05-07 18:39:33 +0900

    8장_6_5-QuerydslRepositorySupport 추상 클래스 사용하기

---
+++ advanced_jpa/src/test/java/com/springboot/advanced_jpa/data/repository/support/ProductRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.springboot.advanced_jpa.data.repository.support;
+
+import com.springboot.advanced_jpa.data.entity.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class ProductRepositoryTest {
+    @Autowired
+    ProductRepository productRepository;
+
+    // advanced_jpa.data.repository.support 패키지의 ProductRepository의 findByName() 메서드 테스트
+    @Test
+    void findByNameTest() {
+        List<Product> productList = productRepository.findByName("펜");
+
+        for (Product product : productList) {
+            System.out.println(product.getNumber());
+            System.out.println(product.getName());
+            System.out.println(product.getPrice());
+            System.out.println(product.getStock());
+        }
+    }
+}
