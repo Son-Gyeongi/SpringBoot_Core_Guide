0f8ad9a3fd31fa7e4461c9b2ea3bc2c9e6e41d8c
Son-Gyeongi <thsruddl77@gmail.com>
2024-05-13 11:39:58 +0900

    10장_유효성 검사와 예외처리 기본 세팅

---
+++ valid_exception/src/main/java/com/springboot/valid_exception/data/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.springboot.valid_exception.data.repository;
+
+import com.springboot.test.data.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// Product 테이블과 매핑되는 Product 엔티티에 대한 인터페이스
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
